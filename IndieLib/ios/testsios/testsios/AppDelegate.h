84d17d35162e3f00a341896e072c847925ba04c3
Miquel Angel Quinones Garcia <m.quinones.garcia@gmail.com>
2013-06-16 20:51:45 +0100

    [ios]Initial XCode project, barebones to link against dependencies

---
+++ IndieLib/ios/testsios/testsios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testsios
+//
+//  Created by Miquel Angel Quinones Garcia on 6/16/13.
+//  Copyright (c) 2013 indielib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IndieLib/ios/testsios/testsios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testsios
+//
+//  Created by Miquel Angel Quinones Garcia on 6/16/13.
+//  Copyright (c) 2013 indielib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

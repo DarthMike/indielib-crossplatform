f5d5d60a07d728049290567b1203c0ead44a2699
Marc-Andre Laperle <malaperle@gmail.com>
2013-08-03 17:59:34 -0400

    Add batch file to build dependencies

---
+++ IndieLib/win/build_deps_vc2008.bat
@@ -0,0 +1,10 @@
+SET DEVENVPATH="C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com"
+SET DEPENDENCIES_PATH=..\common\dependencies
+%DEVENVPATH% %DEPENDENCIES_PATH%\FreeImage\FreeImage.2008.sln /build "Debug|Win32" /Project FreeImageLib /ProjectConfig "Debug|Win32"
+%DEVENVPATH% %DEPENDENCIES_PATH%\FreeImage\FreeImage.2008.sln /build "Release|Win32" /Project FreeImageLib /ProjectConfig "Debug|Win32"
+
+%DEVENVPATH% %DEPENDENCIES_PATH%\glew-1.9.0\build\vc9\glew_static.sln /build "Debug|Win32"
+%DEVENVPATH% %DEPENDENCIES_PATH%\glew-1.9.0\build\vc9\glew_static.sln /build "Release|Win32"
+
+%DEVENVPATH% %DEPENDENCIES_PATH%\SDL-2.0\VisualC\SDL_VS2008.sln /build "Debug|Win32"
+%DEVENVPATH% %DEPENDENCIES_PATH%\SDL-2.0\VisualC\SDL_VS2008.sln /build "Release|Win32"

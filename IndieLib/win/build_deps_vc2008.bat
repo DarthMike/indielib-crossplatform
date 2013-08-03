8703557edd6be7fafbf1817d4d2e7df5c6da9a2d
Marc-Andre Laperle <malaperle@gmail.com>
2013-08-03 19:07:39 -0400

    Fix FreeImage not building in Release

--- IndieLib/win/build_deps_vc2008.bat
+++ IndieLib/win/build_deps_vc2008.bat
@@ -1,7 +1,7 @@
 SET DEVENVPATH="C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com"
 SET DEPENDENCIES_PATH=..\common\dependencies
 %DEVENVPATH% %DEPENDENCIES_PATH%\FreeImage\FreeImage.2008.sln /build "Debug|Win32" /Project FreeImageLib
-%DEVENVPATH% %DEPENDENCIES_PATH%\FreeImage\FreeImage.2008.sln /build "Debug|Win32" /Project FreeImageLib
+%DEVENVPATH% %DEPENDENCIES_PATH%\FreeImage\FreeImage.2008.sln /build "Release|Win32" /Project FreeImageLib
 
 %DEVENVPATH% %DEPENDENCIES_PATH%\glew-1.9.0\build\vc9\glew_static.sln /build "Debug|Win32"
 %DEVENVPATH% %DEPENDENCIES_PATH%\glew-1.9.0\build\vc9\glew_static.sln /build "Release|Win32"

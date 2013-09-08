fb012853d22a00e77ce5c1abe75b14e929bed239
M-F-K <michael@visualdesign.dk>
2013-09-08 21:41:28 +0200

    [engine]Tutorial setup for truetype support.

---
+++ IndieLib/tutorials/basic/17_IND_TTF_Font/CIndieLib.h
@@ -0,0 +1,55 @@
+/*****************************************************************************************
+/* Desc: IndieLib singleton initialization class
+/*****************************************************************************************/
+
+#ifndef _CINDIELIB_
+#define _CINDIELIB_
+
+#pragma comment (lib, "../../common/IndieLib_vc2008.lib")
+#include "../../common/LibHeaders/Indie.h"
+
+//Added by Joel Gao 
+//should be easily integrated into the engin
+#include "IND_TTF_FontManager.h"
+
+class CIndieLib 
+{
+public:
+	
+	static CIndieLib* Instance();
+
+	bool Init ();
+	void End ();
+
+	// ----- IndieLib objects -----
+
+	IND_3dMeshManager		*MeshManager;
+	IND_Input				*Input;
+	IND_Window				*Window;
+	IND_Render				*Render;
+	IND_LightManager		*LightManager;
+	IND_ImageManager		*ImageManager; 
+	IND_SurfaceManager		*SurfaceManager;
+	IND_AnimationManager	*AnimationManager;
+	IND_FontManager			*FontManager;
+	IND_Entity2dManager		*Entity2dManager;
+	IND_Entity3dManager		*Entity3dManager;
+	IND_Math				*Math;
+	//Added by Joel Gao 
+	IND_TTF_FontManager		*TTFFontManager;
+
+protected:
+
+	CIndieLib()  {}
+	CIndieLib(const CIndieLib&);
+	CIndieLib& operator = (const CIndieLib&);
+
+private:
+	static CIndieLib *pinstance;
+
+	void ResetCurrentDirectory_W( void );
+
+};
+
+
+#endif // _CINDIELIB_

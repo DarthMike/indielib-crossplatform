39b843a1b39f824b6bc4d686761adb147183df5d
M-F-K <michael@visualdesign.dk>
2013-09-29 23:01:32 +0200

    [tutorials]added new license

--- IndieLib/tutorials/benchmark/01_Alien_BenchMark/CTerrain.h
+++ IndieLib/tutorials/benchmark/01_Alien_BenchMark/CTerrain.h
@@ -3,6 +3,32 @@
  * Desc: Tiled terrain
  *****************************************************************************************/
 
+/*********************************** The zlib License ************************************
+ *
+ * Copyright (c) 2013 Indielib-crossplatform Development Team
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ * distribution.
+ *
+ *****************************************************************************************/
+
+
 #ifndef _CTERRAIN_
 #define _CTERRAIN_
 
@@ -27,16 +53,16 @@ public:
 
 	~CTerrain();
 
-	IND_Entity2d **mHoles;		// Craters
+	IND_Entity2d **mHoles;          // Craters
 private:
 
 	// ----- Private ------
 
 	CIndieLib *mI;					// IndieLib object
-	IND_Entity2d *mTerrain;				// Wrapped texture
+	IND_Entity2d *mTerrain;			// Wrapped texture
 
 	int mNumHoles;
-	int mNumBlocksX, mNumBlocksY;			// Number of horizontal and vertical tiles
+	int mNumBlocksX, mNumBlocksY;	// Number of horizontal and vertical tiles
 };
 
 

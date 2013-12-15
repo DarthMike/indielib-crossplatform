4012bda9257bcd7d35ecb512ec4edfe12ef5ae05
Miquel Angel Quinones Garcia <m.quinones.garcia@gmail.com>
2013-12-15 20:20:11 +0000

    [gles] Refactors log macro and functions to an implementation file. Fixes #import used in C++ compiler (too much OBJ-C lately ;) )

--- IndieLib/common/src/render/gles/RenderMacros.h
+++ IndieLib/common/src/render/gles/RenderMacros.h
@@ -26,40 +26,12 @@
 #ifndef __INDIELIBIOS_RENDERMACROS_H__
 #define __INDIELIBIOS_RENDERMACROS_H__
 
-#import <string>
-#import <sstream>
+#include <string>
 
 #ifdef DEBUG
-using namespace std;
+extern std::string checkGLError(const char* srcFile, int srcLine);
 
-string checkGLError(const char* srcFile, int srcLine) {
-    GLenum err (glGetError());
-    stringstream errors;
-    
-    while(err!=GL_NO_ERROR) {
-        string error;
-        switch(err) {
-            case GL_INVALID_OPERATION:      error="GL_INVALID_OPERATION";      break;
-            case GL_INVALID_ENUM:           error="GL_INVALID_ENUM";           break;
-            case GL_INVALID_VALUE:          error="GL_INVALID_VALUE";          break;
-            case GL_OUT_OF_MEMORY:          error="GL_OUT_OF_MEMORY";          break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:  error="GL_INVALID_FRAMEBUFFER_OPERATION";  break;
-        }
-        
-        errors << error.c_str() <<" AT - "<<srcFile<<":"<<srcLine<<endl;
-        err=glGetError();
-    }
-    
-    return errors.str();
-}
-
-void logDebugGLError(string error) {
-    if (error.length()) {
-        g_debug->header(error, DebugApi::LogHeaderError);
-    }
-}
-
-#define CHECKGLERRORS() logDebugGLError(checkGLError(__FILE__,__LINE__));
+#define CHECKGLERRORS() //logDebugGLError(checkGLError(__FILE__,__LINE__));
 
 #else
 #define CHECKGLERRORS()

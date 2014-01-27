c899bf443f2340e9d981e313767733063878926f
M_F_K <michael@visualdesign.dk>
2014-01-27 17:29:40 +0100

    [editor]Changed editor license to the zlib license, - Javier said ok to this.

--- IndieLib/tools/editor_tileless/Node.h
+++ IndieLib/tools/editor_tileless/Node.h
@@ -1,19 +1,31 @@
 /*****************************************************************************************
- * Desc: Node class. Each node is a backdrop of the map that contains and IndieLib entity
- * that holds all its attributes
+ * File: Node.h
+ * Desc: Node class. Each node is a backdrop of the map that contains an IndieLib entity
+ *       that holds all its attributes
+ *****************************************************************************************/
+
+/*********************************** The zlib License ************************************
  *
- * gametuto.com - Javier López López (javilop.com)
+ * Copyright (c) 2013 Indielib-crossplatform Development Team
  *
- *****************************************************************************************
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
  *
- * Creative Commons - Attribution 3.0 Unported
- * You are free:
- *	to Share — to copy, distribute and transmit the work
- *	to Remix — to adapt the work
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
  *
- * Under the following conditions:
- * Attribution. You must attribute the work in the manner specified by the author or licensor 
- * (but not in any way that suggests that they endorse you or your use of the work).
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source
+ * distribution.
  *
  *****************************************************************************************/
 
